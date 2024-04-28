544eb32d2eb082a2e73e90a49e9fd0d4bcb7983d
Sophia Xu <shx3@cornell.edu>
2024-04-28 13:20:23 -0400

    recipes added

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from db import Course, User, Assignment, db
+from db import Cuisine, User, Recipe, db
 from flask import Flask, request
 import json
 
@@ -15,49 +15,49 @@ with app.app_context():
 
 # generalized response formats
 def success_response(data, code=200):
-    return json.dumps({"success": True, "courses": data}), code
+    return json.dumps({"success": True, "cuisines": data}), code
 
 def failure_response(message, code=404):
     return json.dumps({"success": False, "error": message}), code
 
 # your routes here
-#get all courses
-@app.route('/api/courses/', methods=['GET'])
-def get_courses():
-    courses = [t.serialize() for t in Course.query.all()]
-    return success_response(courses)
+#get all cuisines
+@app.route('/api/cuisines/', methods=['GET'])
+def get_cuisines():
+    cuisines = [t.serialize() for t in Cuisine.query.all()]
+    return success_response(cuisines)
 
-#create a course
-@app.route("/api/courses/", methods=["POST"])
-def create_course():
+#create a cuisine
+@app.route("/api/cuisines/", methods=["POST"])
+def create_cuisine():
     body = json.loads(request.data)
-    if not body or "code" not in body or "name" not in body:
-        return failure_response("Code or name missing!", 400)
+    if not body or "description" not in body or "name" not in body:
+        return failure_response("Description or name missing!", 400)
     
-    new_course = Course(code=body['code'], name=body['name'])
-    db.session.add(new_course)
+    new_cuisine = Cuisine(description=body['description'], name=body['name'])
+    db.session.add(new_cuisine)
     db.session.commit()
-    return json.dumps(new_course.serialize()), 201
+    return json.dumps(new_cuisine.serialize()), 201
 
-#get a specific course
-@app.route("/api/courses/<int:course_id>/", methods=["GET"])
-def get_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found!")
+#get a specific cuisine
+@app.route("/api/cuisines/<int:cuisine_id>/", methods=["GET"])
+def get_cuisine(cuisine_id):
+    cuisine = cuisine.query.filter_by(id=cuisine_id).first()
+    if cuisine is None:
+        return failure_response("cuisine not found!")
     
-    return json.dumps(course.serialize()), 200
+    return json.dumps(cuisine.serialize()), 200
 
-#delete a course
-@app.route("/api/courses/<int:course_id>/", methods=["DELETE"])
-def delete_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
+#delete a cuisine
+@app.route("/api/cuisines/<int:cuisine_id>/", methods=["DELETE"])
+def delete_cuisine(cuisine_id):
+    cuisine = cuisine.query.filter_by(id=cuisine_id).first()
+    if cuisine is None:
         return failure_response("Task not found!")
     
-    db.session.delete(course)
+    db.session.delete(cuisine)
     db.session.commit()
-    return json.dumps(course.serialize()), 200
+    return json.dumps(cuisine.serialize()), 200
 
 # Create a new user
 @app.route('/api/users/', methods=['POST'])
@@ -81,50 +81,50 @@ def get_user(user_id):
 
     return json.dumps(user.serialize()), 200
 
-# Add a user to a course
-@app.route("/api/courses/<int:course_id>/add/", methods=["POST"])
-def add_user_to_course(course_id):
+# Add a user to a cuisine
+@app.route("/api/cuisines/<int:cuisine_id>/add/", methods=["POST"])
+def add_user_to_cuisine(cuisine_id):
     body = json.loads(request.data)
     if not body or 'user_id' not in body or 'type' not in body:
         return failure_response("Missing required fields: user_id or type", 400)
 
-    course = Course.query.filter_by(id=course_id).first()
-    if not course:
-        return failure_response("Course not found", 404)
+    cuisine = cuisine.query.filter_by(id=cuisine_id).first()
+    if not cuisine:
+        return failure_response("cuisine not found", 404)
 
     user = User.query.get(body['user_id'])
     if not user:
         return failure_response("User not found", 404)
 
-    # Add the user to the course whether student or instructor
+    # Add the user to the cuisine whether student or instructor
     if body['type'] == 'student':
-        course.students.append(user)
+        cuisine.students.append(user)
     elif body['type'] == 'instructor':
-        course.instructors.append(user)
+        cuisine.instructors.append(user)
     else:
         return failure_response("Invalid user type", 400)
 
     db.session.commit()
-    return json.dumps(course.serialize()), 200
+    return json.dumps(cuisine.serialize()), 200
 
-# Create an assignment for a course
-@app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
-def create_assignment_for_course(course_id):
+# Create an recipe for a cuisine
+@app.route("/api/cuisines/<int:cuisine_id>/recipe/", methods=["POST"])
+def create_recipe_for_cuisine(cuisine_id):
     body = json.loads(request.data)
     if not body or 'title' not in body or 'due_date' not in body:
         return failure_response("Missing required fields: title and due_date", 400)
 
-    course = Course.query.filter_by(id=course_id).first()
-    if not course or course is None:
-        return failure_response("Course not found", 404)
+    cuisine = cuisine.query.filter_by(id=cuisine_id).first()
+    if not cuisine or cuisine is None:
+        return failure_response("cuisine not found", 404)
 
     due_date = body['due_date']
 
-    new_assignment = Assignment(title=body['title'], due_date=due_date, course_id=course_id)
-    db.session.add(new_assignment)
+    new_recipe = Recipe(title=body['title'], due_date=due_date, cuisine_id=cuisine_id)
+    db.session.add(new_recipe)
     db.session.commit()
 
-    return json.dumps(new_assignment.serialize()), 201
+    return json.dumps(new_recipe.serialize()), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- src/db.py
+++ src/db.py
@@ -2,68 +2,68 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-association_table_instructor = db.Table('course_instructor_association', db.Model.metadata,
-    db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+association_table_instructor = db.Table('cuisine_instructor_association', db.Model.metadata,
+    db.Column('cuisine_id', db.Integer, db.ForeignKey('cuisine.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-association_table_student = db.Table('course_student_association', db.Model.metadata,
-    db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+association_table_student = db.Table('cuisine_student_association', db.Model.metadata,
+    db.Column('cuisine_id', db.Integer, db.ForeignKey('cuisine.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 # your classes here
-class Course(db.Model):    
-  __tablename__ = "course"    
+class Cuisine(db.Model):    
+  __tablename__ = "cuisine"    
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  code = db.Column(db.String, nullable=False)
+  description = db.Column(db.String, nullable=False)
   name = db.Column(db.String, nullable=False)
-  assignments = db.relationship("Assignment", cascade="delete")
-  instructors = db.relationship("User", secondary='course_instructor_association', back_populates="courses_taught")
-  students = db.relationship("User", secondary='course_student_association', back_populates="courses_enrolled")
+  recipes = db.relationship("Recipe", cascade="delete")
+  instructors = db.relationship("User", secondary='cuisine_instructor_association', back_populates="cuisines_taught")
+  students = db.relationship("User", secondary='cuisine_student_association', back_populates="cuisines_enrolled")
 
   def __init__(self, **kwargs):
-    self.code = kwargs.get("code", "")
+    self.description = kwargs.get("description", "")
     self.name = kwargs.get("name", "")
 
   def serialize(self):    
     return {        
         "id": self.id,
-        "code": self.code,
+        "description": self.description,
         "name": self.name,
-        "assignments": [assignment.simple_serialize() for assignment in self.assignments],
+        "recipes": [recipe.simple_serialize() for recipe in self.recipes],
         "instructors": [instructor.simple_serialize() for instructor in self.instructors],
         "students": [student.simple_serialize() for student in self.students] 
     }
   
-  # Serialize a course without assignments, students, or instructors fields
-  def serialize_course(self):
+  # Serialize a cuisine without recipes, students, or instructors fields
+  def serialize_cuisine(self):
       return {
           "id": self.id,
-          "code": self.code,
+          "description": self.description,
           "name": self.name
       }
 
-class Assignment(db.Model):
-  __tablename__ = "assignment"
+class Recipe(db.Model):
+  __tablename__ = "recipe"
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   title = db.Column(db.String, nullable=False)
   due_date = db.Column(db.Integer, nullable=False)
-  course_id = db.Column(db.Integer, db.ForeignKey("course.id"))
+  cuisine_id = db.Column(db.Integer, db.ForeignKey("cuisine.id"))
 
   def __init__(self, **kwargs):
         self.title= kwargs.get("title")
         self.due_date=kwargs.get("due_date")
-        self.course_id= kwargs.get("course_id")
+        self.cuisine_id= kwargs.get("cuisine_id")
 
   def serialize(self):
-    course = Course.query.filter_by(id=self.course_id).first()
+    cuisine = cuisine.query.filter_by(id=self.cuisine_id).first()
     return {
     'id': self.id,
     'title': self.title,
     'due_date': self.due_date,
-    'course': course.serialize_course()
+    'cuisine': cuisine.serialize_cuisine()
     }
   
   def simple_serialize(self):
@@ -79,29 +79,29 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     netid = db.Column(db.String, nullable=False)
-    courses_taught = db.relationship("Course", secondary=association_table_instructor, back_populates="instructors")
-    courses_enrolled = db.relationship("Course", secondary=association_table_student, back_populates="students")
+    cuisines_taught = db.relationship("Cuisine", secondary=association_table_instructor, back_populates="instructors")
+    cuisines_enrolled = db.relationship("Cuisine", secondary=association_table_student, back_populates="students")
 
     def __init__(self,**kwargs):
         self.name= kwargs.get("name")
         self.netid= kwargs.get("netid")
 
     def serialize(self):
-        enrolled_course_ids = [course.id for course in self.courses_enrolled]
-        taught_course_ids = [course.id for course in self.courses_taught]
-        all_course_ids = enrolled_course_ids + taught_course_ids
+        enrolled_cuisine_ids = [cuisine.id for cuisine in self.cuisines_enrolled]
+        taught_cuisine_ids = [cuisine.id for cuisine in self.cuisines_taught]
+        all_cuisine_ids = enrolled_cuisine_ids + taught_cuisine_ids
         
-        courses = []
-        for course_id in all_course_ids:
-            course = Course.query.filter_by(id=course_id).first()
-            if course:
-                courses.append(course.serialize_course())
+        cuisines = []
+        for cuisine_id in all_cuisine_ids:
+            cuisine = cuisine.query.filter_by(id=cuisine_id).first()
+            if cuisine:
+                cuisines.append(cuisine.serialize_cuisine())
         
         return {
             "id": self.id,
             "name": self.name,
             "netid": self.netid,
-            "courses": courses
+            "cuisines": cuisines
         }
     
     def simple_serialize(self):
